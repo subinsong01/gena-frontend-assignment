2dc971685c79ae8fc222ea3e74d44c5ebba8f8e0
subinsong01 <haely97@naver.com>
2025-07-20 17:58:35 +0900

    style: Add tailwind color & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}", // 컴포넌트 폴더 경로
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#15D798",
+        "primary-color-hover": "#13B47A",
+      },
+      fontFamily: {
+        sans: ['"Pretendard"', "sans-serif"],
+        pretendard: ['"Pretendard"', "sans-serif"],
+        title: ['"JalnanGothic"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
