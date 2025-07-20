79551fe788e330049bbb1e9310cf0e7728061686
subinsong01 <haely97@naver.com>
2025-07-20 19:19:55 +0900

    feat: create InputBox components

---
+++ src/components/InputBox.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface InputBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  label: string;
+  type?: string;
+}
+
+export default function InputBox({
+  id,
+  label,
+  type = "text",
+  ...props
+}: InputBoxProps) {
+  return (
+    <div className="flex flex-col mb-2">
+      <label htmlFor={id} className="mb-2 font-bold text-3xl">
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        {...props}
+        className="border rounded px-3 py-2 focus:outline-none focus:ring-1 focus:ring-primary-color"
+      />
+    </div>
+  );
+}
