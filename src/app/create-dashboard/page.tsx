708135444e3df8ccbb676654e32cc9947ce1e157
subinsong01 <haely97@naver.com>
2025-07-24 15:54:58 +0900

    fix: change folder name

---
+++ src/app/create-dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import DashboardForm from "@/components/DashboardForm";
+
+export default function NewDashboardPage() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold mb-6">Create New Dashboard</h1>
+      <DashboardForm />
+    </main>
+  );
+}
