e3a2954460ea84dde56cedf8aa0bb311086538a1
subinsong01 <haely97@naver.com>
2025-07-24 15:30:15 +0900

    feat: create spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-10 h-10 border-5 border-primary-color border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
