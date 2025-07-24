f5774bac1239c2bb125ab6c90a8293ee7f9c0da9
subinsong01 <haely97@naver.com>
2025-07-24 15:28:52 +0900

    feat: create InputBox component

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -14,14 +14,14 @@ export default function InputBox({
 }: InputBoxProps) {
   return (
     <div className="flex flex-col mb-2">
-      <label htmlFor={id} className="mb-4 font-bold text-xl">
+      <label htmlFor={id} className="mb-5 font-bold text-4xl">
         {label}
       </label>
       <input
         id={id}
         type={type}
         {...props}
-        className="border rounded px-3 py-2 focus:outline-none focus:ring-1 focus:ring-primary-color"
+        className="border rounded px-3 py-3 focus:outline-none focus:ring-1 focus:ring-primary-color"
       />
     </div>
   );
