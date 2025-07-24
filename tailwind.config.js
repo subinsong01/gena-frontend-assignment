6fe821a528799e50b7a69cf337fa2bc01e5a4976
subinsong01 <haely97@naver.com>
2025-07-24 20:07:07 +0900

    style: add tailwind custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
       colors: {
         "primary-color": "#15D798",
         "primary-color-hover": "#13B47A",
+        "edit-color": "#2563eb",
+        "edit-hover-color": "	#1d4ed8",
         "error-color": "#ef4444",
+        "error-color-hover": "#dc2626",
+        "information-color": "#6B7280",
+        "input-box-color": "#D1D5DB",
       },
       fontFamily: {
         sans: ['"Pretendard"', "sans-serif"],
