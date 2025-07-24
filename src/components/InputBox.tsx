15f5ae327a972590b19983cbc81e71a28d5ff1d2
subinsong01 <haely97@naver.com>
2025-07-24 15:49:18 +0900

    style: improve style of inputbox

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -14,14 +14,27 @@ export default function InputBox({
 }: InputBoxProps) {
   return (
     <div className="flex flex-col mb-2">
-      <label htmlFor={id} className="mb-5 font-bold text-4xl">
+      <label htmlFor={id} className="mb-3 text-xl">
         {label}
       </label>
       <input
         id={id}
         type={type}
         {...props}
-        className="border rounded px-3 py-3 focus:outline-none focus:ring-1 focus:ring-primary-color"
+        className="
+        border border-gray-300 rounded-lg px-4 py-3
+        shadow-sm
+        transition
+        duration-300
+        ease-in-out
+        focus:outline-none
+        focus:ring-1
+        focus:ring-primary-color
+        focus:border-primary-color
+        hover:border-primary-color
+        placeholder-gray-400
+        text-gray-900
+      "
       />
     </div>
   );
